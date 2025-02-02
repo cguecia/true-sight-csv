61ec46d799a852131248f333f0faa0f223a505e3
Chris Guecia <cguecia11@gmail.com>
2025-02-02 15:26:38 -0500

    Adds validations for provided csv full path. Updates main to use validation on parsed arg. Adds sample csv file to tests dir.

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::error::Error;
 use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
@@ -6,4 +7,24 @@ use std::path::PathBuf;
 pub struct TrueSightCsvArgs {
     /// The full path to the csv file to be inspected
     pub file_full_path: PathBuf,
+}
+
+impl TrueSightCsvArgs {
+    pub fn validate_csv_path(&self) -> Result<&PathBuf, Box<dyn Error>> {
+        // Check existence first
+        if !self.file_full_path.exists() {
+            return Err("File does not exist".into());
+        }
+
+        // Check if it's actually a file (not a directory)
+        if !self.file_full_path.is_file() {
+            return Err("Path exists but is not a file".into());
+        }
+
+        // Check file extension
+        match self.file_full_path.extension() {
+            Some(ext) if ext == "csv" => Ok(&self.file_full_path),
+            _ => Err("File must have .csv extension".into()),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod args;
 use args::TrueSightCsvArgs;
 use clap::Parser;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: TrueSightCsvArgs = TrueSightCsvArgs::parse();
     println!("Provided full path to file: {:?}", &args);
+    args.validate_csv_path()?;
+    Ok(())
 }
