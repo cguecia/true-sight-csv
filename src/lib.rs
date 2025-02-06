4c21f31ad503c662133f993d9fadaeedbd9b5d45
Chris Guecia <cguecia11@gmail.com>
2025-02-06 12:38:06 -0500

    Adds ways to read in chunks of csv using iterators.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,88 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+use csv::Reader;
+use prettytable::{Cell, Row, Table};
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+
+pub fn pretty_print(file_path: &std::path::PathBuf) -> Result<(), Box<dyn Error>> {
+    // TODO: pretty print is currently using a reader to read the csv file
+    // other things are going to read the file as well, this should really onnly be done 1 time
+    let mut rdr = csv::Reader::from_path(file_path)?;
+    let headers = rdr.headers()?;
+
+    let mut table = Table::new();
+    table.add_row(Row::from_iter(headers.iter().map(|h| Cell::new(h))));
+
+    for result in rdr.records().take(10) {
+        let record = result?;
+        table.add_row(Row::from_iter(record.iter().map(|field| Cell::new(field))));
+    }
+
+    table.printstd();
+    Ok(())
+}
+
+pub fn read_csv_chunks(path: &PathBuf, chunk_size: usize) -> Result<(), Box<dyn Error>> {
+    let file = File::open(path)?;
+    // TODO: Check docks for ReaderBuilder to add more customiztion for example toggle has_headers bool true or false
+    let mut rdr = Reader::from_reader(file);
+
+    let mut records = rdr.records();
+
+    // TODO: Think of a better way to send the process into the loop
+    // Should this return the chunk or pointer to the chunk then continue like a python yield?
+
+    while let Some(chunk) = records
+        .by_ref()
+        .take(chunk_size)
+        .collect::<Result<Vec<_>, _>>()
+        .ok()
+    {
+        if chunk.is_empty() {
+            break;
+        }
+        println!("Processing chunk of {} records", chunk.len());
+    }
+    Ok(())
+}
+
+// Trying out making an iterator that can read in chunks with csv Reader
+pub struct CsvChunkIterator<'a, R: Read> {
+    records: csv::StringRecordsIter<'a, R>, // 'a is the lifetime specifier compiler is asking for this
+    chunk_size: usize,
+}
+
+impl<'a, R: Read> CsvChunkIterator<'a, R> {
+    pub fn new(records: csv::StringRecordsIter<'a, R>, chunk_size: usize) -> Self {
+        CsvChunkIterator {
+            records,
+            chunk_size,
+        }
+    }
+}
+
+impl<R: Read> Iterator for CsvChunkIterator<'_, R> {
+    type Item = Result<Vec<csv::StringRecord>, csv::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let chunk: Result<Vec<_>, _> = self.records.by_ref().take(self.chunk_size).collect(); // this essentially allows for the iterator not to reset and read from top of file again
+
+        match chunk {
+            Ok(records) if records.is_empty() => None, // End of iterator, no more chunks
+            Ok(records) => {
+                // Print a message when a chunk is read
+                println!("Chunk read with {} records", records.len());
+                Some(Ok(records))
+            }
+            Err(e) => Some(Err(e)), // Propagate the error if there was one
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod args;
 
 use args::TrueSightCsvArgs;
 use clap::Parser;
+use csv::ReaderBuilder;
+use std::fs::File;
+use true_sight_csv::CsvChunkIterator;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: TrueSightCsvArgs = TrueSightCsvArgs::parse();
@@ -9,5 +12,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let validated_path: &std::path::PathBuf = args.validate_csv_path()?;
     println!("Valid CSV path: {:?}", validated_path);
+
+    // Open the file and create a Reader
+    let file: File = File::open(validated_path)?;
+    let mut rdr: csv::Reader<File> = ReaderBuilder::new().from_reader(file);
+
+    let chunk_iterator: CsvChunkIterator<'_, File> = CsvChunkIterator::new(rdr.records(), 100_000);
+
+    // Initialize a counter for the total row count
+    let mut total_row_count = 0;
+
+    // Iterate over the chunks and process them
+    for chunk in chunk_iterator {
+        match chunk {
+            Ok(records) => {
+                total_row_count += records.len();
+                // Process the chunk here (e.g., perform QA checks)
+                println!("Processed chunk of size {}", records.len());
+            }
+            Err(e) => {
+                eprintln!("Error reading CSV: {}", e);
+                break;
+            }
+        }
+    }
+
+    // TODO: build out way to read in the csv in chunks if file is large
+    //
+
+    //read_csv_chunks(validated_path, 2);
+
+    //let mut reader = csv::Reader::from_path(validated_path)?;
+
+    // Print all records
+    //for result in reader.records() {
+    //    let record = result?;
+    //    println!("{:?}", record);
+    //}
+
+    // Print in formatted table stdout
+    //pretty_print(validated_path)?;
+
+    // TODO: make the csv checker with different checks for nulls or NaN ect.
+    // strategey pattern for adding new checks easier
+
+    // After processing all chunks, print the total row count
+    println!("Total row count: {}", total_row_count);
+
     Ok(())
 }
